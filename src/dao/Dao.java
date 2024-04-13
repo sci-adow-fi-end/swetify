ec8dbdfcc784e4ec2950d392301daf01b66a088b
Filippo Viti <filippo.viti2002@gmail.com>
2024-04-13 19:38:05 +0200

    Moved DAO to its own package

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,4 +1,4 @@
-package domainmodel;
+package dao;
 
 import java.util.List;
 import java.util.Optional;
---
+++ src/dao/UserDao.java
@@ -0,0 +1,37 @@
+package dao;
+
+import domainmodel.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserDao implements Dao<User> {
+
+    public UserDao() {
+    }
+
+    @Override
+    public Optional<User> get(long id) {
+        // code to select the user from the database
+    }
+
+    @Override
+    public List<User> getAll() {
+        // code to select all users from the database
+    }
+
+    @Override
+    public void save(User user) {
+        // code to save the user into the database
+    }
+
+    @Override
+    public void update(User user, String[] params) {
+        // code to update the user in the database
+    }
+
+    @Override
+    public void delete(User user) {
+        // code to delete the user from the database
+    }
+}
