f73d0e56005d329bac2eff0c3ff322b95e3015c0
Capitan_Uncino <alessio.delli@stud.unifi.it>
2024-04-17 18:37:26 +0200

    added printError function

--- src/main/java/businesslogic/Handler.java
+++ src/main/java/businesslogic/Handler.java
@@ -6,11 +6,28 @@ public abstract class Handler {
 
     protected abstract void pullData();
     protected abstract void render();
-    protected abstract void handleInput();
+    protected abstract boolean handleInput();
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    protected static void printError(){
+        System.out.println(" ");
+        System.out.println("the inserted input is not valid, please type it again");
+        System.out.println(" ");
+    }
 
     public void update(){
         pullData();
         render();
-        handleInput();
+        boolean askForInput = true;
+        while (askForInput) {
+            askForInput = handleInput();
+            if (askForInput){
+                printError();
+            }
+        }
     }
 }
--- src/main/java/businesslogic/HomeHandler.java
+++ src/main/java/businesslogic/HomeHandler.java
@@ -2,14 +2,21 @@ package businesslogic;
 
 public class HomeHandler extends Handler {
 
+
     @Override
     protected void pullData() {
     }
 
     @Override
     protected void render() {
+        clearScreen();
+        System.out.println("welcome to swetify, the engeneer's music streaming service!");
+        System.out.println("\n");
+        System.out.println("press 1 to search for a track, artist or playlist");
+        System.out.println("press 2 to view your playlists");
+        System.out.println("press 3 to view suggested songs");
+        System.out.println("press 4 to log out");
     }
-
     @Override
     protected void handleInput() {
     }
