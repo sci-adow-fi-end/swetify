f6aaa5095827f3317c2c38f32b71bd2d3421081c
Filippo Viti <filippo.viti2002@gmail.com>
2024-04-18 13:06:05 +0200

    Fixed generated code

--- src/main/java/domainmodel/PlaybackQueue.java
+++ src/main/java/domainmodel/PlaybackQueue.java
@@ -20,19 +20,17 @@ public class PlaybackQueue {
 
     public void addTrack(Track track) {
         // TODO implement here
-        return null;
     }
 
 
     public void addPlaylist(Playlist playlist) {
         // TODO implement here
-        return null;
+
     }
 
 
     public void clear() {
         // TODO implement here
-        return null;
     }
 
 }
