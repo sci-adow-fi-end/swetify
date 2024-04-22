454d11229522c8ef97ef2185d70e0c2f0a8dd77e
Capitan_Uncino <alessio.delli@stud.unifi.it>
2024-04-22 23:31:02 +0200

    added some interfaces

--- src/main/java/businesslogic/LoginHandler.java
+++ src/main/java/businesslogic/LoginHandler.java
@@ -1,8 +1,19 @@
 package businesslogic;
 
+import dao.Dao;
+import domainmodel.User;
+
 public class LoginHandler extends Handler{
+
+    Dao<User> =
+
+    public String userName;
+    public String password;
+
+
     @Override
     protected void pullData() {
+        id =
     }
 
     @Override
--- src/main/java/businesslogic/SearchHandler.java
+++ src/main/java/businesslogic/SearchHandler.java
@@ -1,12 +1,25 @@
 package businesslogic;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import dao.Dao;
+import domainmodel.Song;
+
 public class SearchHandler extends Handler {
+
+    private String input = "";
+    private Optional songs;
+    private Dao database;
+
     @Override
     protected void pullData() {
+        songs = database.get(input);
     }
 
     @Override
     protected void render() {
+
     }
 
     @Override
