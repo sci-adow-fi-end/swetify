104be3ee398ce64fa6cca6407541263d2c2b2223
Capitan_Uncino <alessio.delli@stud.unifi.it>
2024-04-23 20:49:20 +0200

    updated SearchHandler

--- src/main/java/businesslogic/Handler.java
+++ src/main/java/businesslogic/Handler.java
@@ -2,6 +2,17 @@ package businesslogic;
 
 public abstract class Handler {
 
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     private static NavigationManager navigationManager = new NavigationManager();
 
     protected abstract void pullData();
--- src/main/java/businesslogic/SearchHandler.java
+++ src/main/java/businesslogic/SearchHandler.java
@@ -5,20 +5,32 @@ import java.util.Optional;
 
 import dao.Dao;
 import domainmodel.Song;
+import domainmodel.Track;
 
 public class SearchHandler extends Handler {
 
     private String input = "";
-    private Optional songs;
+    private ArrayList<Song> songs;
+    private ArrayList<Song> podcasts ;
+    private ArrayList<Song> artists;
     private Dao database;
 
     @Override
     protected void pullData() {
-        songs = database.get(input);
+        try {
+            songs = (ArrayList<Song>) database.get(input).orElseThrow();
+        }catch (Exception e){
+            System.out.println(ANSI_RED +"No matches found"+ ANSI_RESET);
+        }
     }
 
     @Override
     protected void render() {
+        System.out.println("enter a keyword to search or the number of a track to select it \n");
+        int i = 0;
+        for (Song song : songs){
+            System.out.println(i+") "+ song.getTitle());
+        }
 
     }
 
