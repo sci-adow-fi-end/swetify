789599e8c28c55672419ac40ec49604f54a82381
sci_adow_fi_end <alessio.delli@edu.unifi.it>
2024-07-01 17:02:08 +0200

    updated playlist handler

--- src/main/java/businesslogic/PlaylistHandler.java
+++ src/main/java/businesslogic/PlaylistHandler.java
@@ -6,11 +6,47 @@ public class PlaylistHandler extends Handler {
         System.out.println("0: go back");
         System.out.println("1: add playlist to the bottom of the queue");
         System.out.println("2: add playlist to the top of the queue");
+	System.out.println("3: add song to the playlist");
+	System.out.println("4: remove song from the playlist");
+	
     }
 
     @Override
     public State update(State state) {
+	clearScreen();
+        renderChoices();
+        int navigationOption = -1;
+        boolean validNavigationOption = false;
+        Scanner input = new Scanner(System.in);
+        while (!validNavigationOption) {
+            try {
+                navigationOption = Integer.parseInt(input.nextLine());
+                validNavigationOption = true;
+            } catch (NumberFormatException ignored) {
+                continue;
+            }
 
+            switch (navigationOption) {
+                case 1:
+                    navigationManager.switchToController(NavigationManager.HandlerId.SEARCH);
+                    break;
+                case 2:
+                    navigationManager.switchToController(NavigationManager.HandlerId.VIEW_PLAYLIST);
+                    break;
+                case 3:
+                    navigationManager.switchToController(NavigationManager.HandlerId.VIEW_SUGGESTIONS);
+                    break;
+                case 4:
+                    navigationManager.switchToController(NavigationManager.HandlerId.LOGIN);
+                    break;
+	        case 5:
+		    navigationManager.switchToController(NavigationManager.HandlerId.LOGIN);
+                    break;
+                default:
+                    printError("inserted option not valid");
+                    validNavigationOption=false;
+            }
+        }
         return state;
     }
 }
