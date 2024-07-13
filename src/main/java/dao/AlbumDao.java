02fbfc25afd9d084193259b75a5f1676094b489c
filippo-viti <filippo.viti2002@gmail.com>
2024-07-13 19:29:34 +0200

    Added AlbumDao.java

---
+++ src/main/java/dao/AlbumDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import domainmodel.Album;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AlbumDao extends Dao<Album> {
+
+    @Override
+    public Optional<Album> get(long id) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        return Optional.ofNullable(entityManager.find(Album.class, id));
+    }
+
+    @Override
+    public Album getByName(String name) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        TypedQuery<Album> query = entityManager.createQuery("SELECT a FROM Album a JOIN a.playlist p WHERE p.title = :name", Album.class);
+        query.setParameter("name", name);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public List<Album> getAll() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        TypedQuery<Album> query = entityManager.createQuery("SELECT a FROM Album a", Album.class);
+        return query.getResultList();
+    }
+}
