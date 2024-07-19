9377dcdad31f8a70cdce90bb47f8e8adbf0f1049
filippo-viti <filippo.viti2002@gmail.com>
2024-07-19 09:48:42 +0200

    Cleaned up main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,8 @@
 import businesslogic.utility.ConfigOptions;
 import businesslogic.utility.NavigationManager;
-import dao.tracks.SongDAO;
-import dao.users.ArtistDAO;
-import dao.users.CustomerDAO;
-import domainmodel.entities.tracks.Song;
-import domainmodel.entities.users.Artist;
-import domainmodel.entities.users.Customer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        SongDAO songDao = new SongDAO();
-        ArtistDAO artistDao = new ArtistDAO();
-        CustomerDAO customerDao = new CustomerDAO();
-        Customer customer = new Customer("stio", "stio");
-        customerDao.save(customer);
-        Artist artist = new Artist("Stio", "Stio", "StioNelCHill", "no biography for you, asshole");
-        artistDao.save(artist);
-        List<Artist> authors = new ArrayList<>();
-        authors.add(artist);
-        Song song = new Song("Stio", "KPop", 1, 10, authors);
-        songDao.save(song);
-
         ConfigOptions.TEST_MODE = false;
         NavigationManager nm = new NavigationManager();
     }
